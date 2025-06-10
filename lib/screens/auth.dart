0563ce65c2b0b6e30b10b55474784a9b3d0f2193
singhr45625 <kpuja0969@gmail.com>
2025-06-09 23:59:35 -0300

    first commit

---
+++ lib/screens/auth.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+
+  void _submit() {
+    final isValid = _form.currentState!.validate();
+
+    if(isValid) {
+      _form.currentState!.save();
+      print(_enteredEmail);
+      print(_enteredPassword);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: 30,
+                  bottom: 20,
+                  left: 20,
+                  right: 20,
+                ),
+                width: 200,
+                child: Image.asset('assets/images/chat1.png'),
+              ),
+              Card(
+                margin: const EdgeInsets.all(20),
+                child : SingleChildScrollView(
+                  child: Padding(
+                      padding: EdgeInsets.all(15),
+                    child: Form(
+                      key: _form,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: "Email Address",
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                              autocorrect: false,
+                              textCapitalization: TextCapitalization.none,
+                              validator: (value) {
+                                  if( value == null || value.trim().isEmpty || !value.contains('@')) {
+                                    return 'Please enter a valid email address.';
+                                  }
+                                  return null;
+                              },
+                                onSaved: (value) {
+                                  _enteredEmail = value!;
+                                }
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: "Password",
+                              ),
+                              obscureText: true,
+                              validator: (value) {
+                                if( value == null || value.trim().length < 6) {
+                                  return 'Please enter atleast 6 character password.';
+                                }
+                                return null;
+                              },
+                                onSaved: (value) {
+                                  _enteredPassword = value!;
+                                }
+                            ),
+                            SizedBox(height: 12,),
+                            ElevatedButton(
+                              onPressed:  _submit,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+                              ),
+                                child: Text(_isLogin ? "LogIn" : "SingUp"),
+                            ),
+                            TextButton(
+                                onPressed: () {
+                              setState(() {
+                                 _isLogin = !_isLogin;
+                            });},
+                                child: Text(_isLogin ? "Create an account" : "I have already an account")
+                            ),
+                          ],
+                        ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
